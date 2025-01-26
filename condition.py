1f82acf8b8a83e1269d39550a6f6aa170a2d596e
Choion <jahidulhasan01754@gmail.com>
2025-01-26 20:38:45 +0600

    conditional statement

--- condition.py
+++ condition.py
@@ -1,6 +1,17 @@
 x = 5
 if x < 10:
     print('Smaller')
+
 if x > 20:
     print('Bigger')
+    print('Again Bigger')
+
+print('hey')
 print('Finish')
+
+x = 4
+if x > 2:
+    print('Bigger')
+else :
+    print('Smaller')
+print('All done')
