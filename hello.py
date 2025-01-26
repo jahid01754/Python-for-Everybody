e046ef4742bf6b446df615485db4d2a4acd0a093
Choion <jahidulhasan01754@gmail.com>
2025-01-26 20:17:35 +0600

    first commit

---
+++ hello.py
@@ -0,0 +1,44 @@
+print('Hello world')
+
+x = 1
+print(x)
+
+x = x + 1
+print(x)
+
+n = 5
+while n > 0 :
+    print(n)
+    n = n - 1
+print('Blastoff!')
+
+jahid = 2
+hasan = 10
+
+choion = hasan ** jahid
+print(choion)
+
+# print(eee = 'Jahidul ' +  ' Hasan')
+eee = 'Jahidul ' +  ' Hasan'
+print(eee)
+print('Jahidul ' + 'Hasan')
+
+type (eee)
+
+type('Hello')
+print(type('hello'))
+print(10/2)
+
+name = input('who are you?') 
+print('welcome', name)
+
+inp = input('Europe floor?')
+usf = int(inp) +1
+print('US floor', usf)
+
+x = 5
+if x < 10:
+    print('smaller')
+if x > 20:
+    print('Bigger')
+print('Finish')
